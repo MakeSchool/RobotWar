854533cb86c9c13c23da7bc90795d5e4f33469e5
Ben-G <Benjamin.Encz@gmail.com>
2014-05-30 10:51:25 -0700

    dispatch 'runAction' back to main queue

--- RobotWar.spritebuilder/Source/Robot.m
+++ RobotWar.spritebuilder/Source/Robot.m
@@ -14,68 +14,48 @@ static CGFloat const ROBOT_DEGREES_PER_SECOND = 60;
     CCNode *_barell;
 }
 
-- (void)turnGunLeft:(NSInteger)degree {
-  
-  // each robot can only perform operations on his own queue!
-  NSAssert(dispatch_get_current_queue() == self.operationQueue, @"You're trying to cheat? Your robot is only allowed to use his own queue!");
-  
-    __block BOOL complete = FALSE;
-    
-    CGFloat currentRotation = _barell.rotation;
-    CGFloat duration = degree / ROBOT_DEGREES_PER_SECOND;
-    
-    CCActionRotateTo *rotateTo = [CCActionRotateTo actionWithDuration:duration angle:currentRotation-degree];
-    CCActionCallBlock *actionCallBlock = [CCActionCallBlock actionWithBlock:^{
-        complete = TRUE;
-    }];
-    
-    CCActionSequence *sequence = [CCActionSequence actionOne:rotateTo two:actionCallBlock];
-    
-    [_barell runAction:sequence];
-    
-    while (!complete) {
-        
-    }
-    
-    return;
-}
-
-- (void)turnGunRight:(NSInteger)degree {
-  
-  
+- (void)performRobotAction:(CCActionFiniteTime*)action {
   // each robot can only perform operations on his own queue!
   NSAssert(dispatch_get_current_queue() == self.operationQueue, @"You're trying to cheat? Your robot is only allowed to use his own queue!");
   
   __block BOOL complete = FALSE;
+
   
-  CGFloat currentRotation = _barell.rotation;
-  CGFloat duration = degree / ROBOT_DEGREES_PER_SECOND;
-  
-  CCActionRotateTo *rotateTo = [CCActionRotateTo actionWithDuration:duration angle:currentRotation+degree];
   CCActionCallBlock *actionCallBlock = [CCActionCallBlock actionWithBlock:^{
     complete = TRUE;
   }];
   
-  CCActionSequence *sequence = [CCActionSequence actionOne:rotateTo two:actionCallBlock];
+  CCActionSequence *sequence = [CCActionSequence actionOne:action two:actionCallBlock];
   
-  [_barell runAction:sequence];
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [_barell runAction:sequence];
+  });
   
-  while (!complete) {
-    
-  }
+  while (!complete);
   
   return;
 }
 
+- (void)turnGunLeft:(NSInteger)degree {
+  
+    CGFloat currentRotation = _barell.rotation;
+    CGFloat duration = degree / ROBOT_DEGREES_PER_SECOND;
+    
+    CCActionRotateTo *rotateTo = [CCActionRotateTo actionWithDuration:duration angle:currentRotation-degree];
+    [self performRobotAction:rotateTo];
+}
+
+- (void)turnGunRight:(NSInteger)degree {
+  CGFloat currentRotation = _barell.rotation;
+  CGFloat duration = degree / ROBOT_DEGREES_PER_SECOND;
+  
+  CCActionRotateTo *rotateTo = [CCActionRotateTo actionWithDuration:duration angle:currentRotation+degree];
+  [self performRobotAction:rotateTo];
+}
+
 - (void)performAction {
   [self turnGunLeft:180];
-  
-  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-    NSLog(@"Trying to break this!");
-    [self turnGunRight:180];
-  });
-  
-  
+  [self turnGunRight:20];
   NSLog(@"Turn completed");
 }
 
