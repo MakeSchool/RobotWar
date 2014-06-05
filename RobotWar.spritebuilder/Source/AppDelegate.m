e9c7184b55b6bb99734949216a56e4db8bcc89e7
Ben-G <Benjamin.Encz@gmail.com>
2014-06-05 16:46:46 -0700

    make robot class and name externally configurable

--- RobotWar.spritebuilder/Source/AppDelegate.m
+++ RobotWar.spritebuilder/Source/AppDelegate.m
@@ -27,6 +27,7 @@
 
 #import "AppDelegate.h"
 #import "CCBuilderReader.h"
+#import "Configuration.h"
 
 @implementation AppController
 
---
+++ RobotWar.spritebuilder/Source/Configuration.h
@@ -0,0 +1,18 @@
+//
+//  Configuration.h
+//  RobotWar
+//
+//  Created by Benjamin Encz on 05/06/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#ifndef RobotWar_Configuration_h
+#define RobotWar_Configuration_h
+
+static NSString *robotClass1 = @"SimpleRobot";
+static NSString *robotName1 = @"Benji's Robot";
+
+static NSString *robotClass2 = @"AdvancedRobot";
+static NSString *robotName2 = @"Jeremy's Robot";
+
+#endif
