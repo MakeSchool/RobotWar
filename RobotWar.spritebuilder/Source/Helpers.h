cb9d3896cb7e7449faff45a099a81c36bf435074
Ben-G <Benjamin.Encz@gmail.com>
2014-06-05 17:28:18 -0700

    restructued project, removed warnings

--- RobotWar.spritebuilder/Source/Helpers.h
+++ RobotWar.spritebuilder/Source/Helpers.h
@@ -11,24 +11,8 @@
 
 #import "Robot.h"
 
-static CGFloat radToDeg(CGFloat rad) {
-  return rad * (180/M_PI);
-}
-
-static CGFloat degToRad(CGFloat deg) {
-  return deg * (M_PI/180);
-}
-
-static RobotWallHitDirection radAngleToRobotWallHitDirection(CGFloat rad) {
-  if (rad >= -135 && rad <= -46) {
-    return RobotWallHitDirectionLeft;
-  } else if ((rad >= 136 && rad <= 180) || (rad >= -180 && rad <= -136)) {
-    return RobotWallHitDirectionFront;
-  } else if  (rad >= 46 && rad <= 135){
-    return RobotWallHitDirectionRight;
-  } else  {
-    return RobotWallHitDirectionRear;
-  }
-}
+extern CGFloat radToDeg(CGFloat rad);
+extern CGFloat degToRad(CGFloat deg);
+extern RobotWallHitDirection radAngleToRobotWallHitDirection(CGFloat rad);
 
 #endif
