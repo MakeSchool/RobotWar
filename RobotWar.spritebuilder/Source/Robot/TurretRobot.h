f4bde78da849f0d17e1f1abc18759af37a338c45
Daniel Haaser <thedapperdan@gmail.com>
2014-06-29 22:21:59 -0700

    TurretRobot part deux

--- RobotWar.spritebuilder/Source/Robot/TurretRobot.h
+++ RobotWar.spritebuilder/Source/Robot/TurretRobot.h
@@ -3,7 +3,7 @@
 //  RobotWar
 //
 //  Created by Daniel Haaser on 6/27/14.
-//  Copyright (c) 2014 Apportable. All rights reserved.
+//  Copyright (c) 2014 MakeGamesWith.Us. All rights reserved.
 //
 
 #import "Robot.h"
