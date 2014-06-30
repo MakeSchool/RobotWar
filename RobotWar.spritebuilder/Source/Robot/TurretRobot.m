d572974b54369286e15a41cd4b380a1d3a855e55
Daniel Haaser <thedapperdan@gmail.com>
2014-06-29 22:17:39 -0700

    Initial commit of Turretbot

---
+++ RobotWar.spritebuilder/Source/Robot/TurretRobot.m
@@ -0,0 +1,13 @@
+//
+//  TurretRobot.m
+//  RobotWar
+//
+//  Created by Daniel Haaser on 6/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "TurretRobot.h"
+
+@implementation TurretRobot
+
+@end
